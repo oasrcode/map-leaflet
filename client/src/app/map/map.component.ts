e67bbdc00e814de7f38afab169655343a14f3e32
oasrcode <oasrcode@gmail.com>
2023-08-27 19:12:36 +0000

    leaflet lts and minor changes

--- client/src/app/map/map.component.ts
+++ client/src/app/map/map.component.ts
@@ -25,19 +25,19 @@ export class MapComponent implements AfterViewChecked {
   }
 
   private initMap() {
-    this.map = new Map('leafletMap').setView(this.coordenadas, 20);
+    if (this.coordenadas != null && this.nombre != null) {
+      this.map = new Map('leafletMap').setView(this.coordenadas, 20);
 
-    tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      maxZoom: 19,
-      attribution:
-        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-    }).addTo(this.map);
+      tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(this.map);
 
-    const marca = marker(this.coordenadas, { title: this.nombre }).addTo(
-      this.map
-    );
-    marca.bindPopup('Calle : ' + this.nombre);
+      const marca = marker(this.coordenadas, { title: this.nombre }).addTo(
+        this.map
+      );
+      marca.bindPopup('Calle : ' + this.nombre);
+    }
   }
-
-  
 }
